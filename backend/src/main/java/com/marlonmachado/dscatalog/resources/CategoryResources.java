ce086b8b30b021b57d9a2260305a6589741bc94c
Marlon Machado <marlon.conceicao.0308@gmail.com>
2025-11-22 10:58:55 -0300

    POST Category

--- backend/src/main/java/com/marlonmachado/dscatalog/resources/CategoryResources.java
+++ backend/src/main/java/com/marlonmachado/dscatalog/resources/CategoryResources.java
@@ -1,15 +1,13 @@
 package com.marlonmachado.dscatalog.resources;
 
 import com.marlonmachado.dscatalog.dto.CategoryDTO;
-import com.marlonmachado.dscatalog.entities.Category;
 import com.marlonmachado.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -31,4 +29,11 @@ public class CategoryResources {
         return ResponseEntity.ok().body(dto);
     }
 
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
 }
