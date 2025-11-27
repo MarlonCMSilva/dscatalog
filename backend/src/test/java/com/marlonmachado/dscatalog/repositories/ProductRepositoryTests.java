9c54f1d0d375f84898ebc50d10c507e80c1fadea
Marlon Machado <marlon.conceicao.0308@gmail.com>
2025-11-27 09:57:40 -0300

    create RepositoryTests

---
+++ backend/src/test/java/com/marlonmachado/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,41 @@
+package com.marlonmachado.dscatalog.repositories;
+
+import com.marlonmachado.dscatalog.entities.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+
+    @Autowired
+    private ProductRepository repository;
+
+    private long existingId;
+    private long nonExistingId;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 10000L;
+    }
+
+    @Test
+    public void deleteShouldDeleteObjectWhenIdExists(){
+
+        repository.deleteById(existingId);
+
+       Optional<Product> result =  repository.findById(existingId);
+
+       Assertions.assertFalse(result.isPresent());
+    }
+
+
+
+}
